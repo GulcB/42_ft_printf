84d6ecd155d395a1a4155ba7029ff072a7ddd613
Gulcin <152058251+GulcB@users.noreply.github.com>
2024-11-08 14:13:27 +0300

    Update ft_printf_funcs.c

--- ft_printf_funcs.c
+++ ft_printf_funcs.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+#include "ft_printf.h"
 
 int	ft_putchar(char c)
 {
@@ -56,4 +56,40 @@ int	ft_putnbr(int nb)
 		i += ft_putnbr(nb % 10);
 	}
 	return (i);
+}
+
+int	ft_print_hex(unsigned long num, int is_uppercase)
+{
+	int		count;
+	char	*base;
+
+	count = 0;
+	if (is_uppercase)
+		base = "0123456789ABCDEF";
+	else
+		base = "0123456789abcdef";
+	if (num >= 16)
+		count += ft_print_hex(num / 16, is_uppercase);
+	count += ft_putchar(base[num % 16]);
+	return (count);
+}
+
+int	ft_uns(unsigned int u)
+{
+	char	buffer[10];
+	int		i;
+	int		count;
+
+	if (u == 0)
+		return (ft_putchar('0'));
+	i = 0;
+	while (u > 0)
+	{
+		buffer[i++] = (u % 10) + '0';
+		u /= 10;
+	}
+	count = i;
+	while (--i >= 0)
+		ft_putchar(buffer[i]);
+	return (count);
 }
